f933d170e8950a36d67e8280db102599f44519f4
Maithri <160866552+Maithri1234@users.noreply.github.com>
2024-04-04 11:56:12 +0530

    Add files via upload

---
+++ IterateArray.java
@@ -0,0 +1,9 @@
+class IterateArray {
+public static void main(String args[]) {
+int[] arr={1,2,4,5,6,7};
+System.out.println("elements are:");
+for (int i=0;i<arr.length;i++) {
+System.out.println(arr[i]);
+}
+}
+}
