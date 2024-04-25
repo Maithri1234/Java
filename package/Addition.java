c86a3f83bdf7a33d9f024d93678862546176e77c
Maithri <160866552+Maithri1234@users.noreply.github.com>
2024-04-25 13:06:01 +0530

    Add files via upload
    programs on packages

---
+++ package/Addition.java
@@ -0,0 +1,12 @@
+package arithmeticop;
+public class Addition {
+private int d1,d2;
+public Addition(int n1, int n2) {
+this.d1=n1;
+this.d2=n2;
+}
+public  void sum()
+{
+System.out.println("sum of two numbers is:"+(d1+d2));
+}
+}
---
+++ package/Substraction.java
@@ -0,0 +1,12 @@
+package arithmeticop;
+public class Substraction {
+private int d1,d2;
+public Substraction(int n1, int n2) {
+this.d1=n1;
+this.d2=n2;
+}
+public  void difference()
+{
+System.out.println("substraction of two numbers is:"+(d1-d2));
+}
+}
